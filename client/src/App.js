9fdc5a218e9343c13c55a4c2caa2b1ee33a0a20f
Chicco Moshe Serby <chiccomoshe@gmail.com>
2020-05-01 21:19:59 +0300

    v1

--- client/src/App.js
+++ client/src/App.js
@@ -31,13 +31,14 @@ class App extends Component {
 
     this.state = {
       projects: [],
-      // hostVar  : '',
-      hostVar: 'http://localhost:5000',
+      hostVar: '',
+      // hostVar: 'http://localhost:5000',
       navItems: [{ name: 'מוסרים באהבה', id: 2, to: "/home", className: "nav_item" },
       { name: 'אודות', id: 1, to: "/About", className: "nav_item" },
       { name: 'מידע', id: 0, to: "/", className: "nav_item" },
-      { name: 'צור קשר', id: 3, to: "/Contact", className: "nav_item" },
-      { name: 'מנהל', id: 4, to: "/Manager", className: "nav_item" }],
+      { name: 'צור קשר', id: 3, to: "/Contact", className: "nav_item" }],
+      // ,
+      // { name: 'מנהל', id: 4, to: "/Manager", className: "nav_item" }],
       activeLink: 0,
 
     }
@@ -114,7 +115,7 @@ class App extends Component {
               <li className={
                 (1 == this.state.activeLink ? "nav-item active_item" : "nav-item")}>
 
-                <Link to="/home" className="nav-link"
+                <Link to="/" className="nav-link"
                   onClick={() => { this.handleClick(1) }}>
                   <img className="navIcon" src="img/home.svg" />
                   <span className=" link-text">מוסרים באהבה</span>
@@ -129,14 +130,14 @@ class App extends Component {
                   <span className=" link-text">צור קשר</span>
                 </Link>
               </li>
-              <li className={
+              {/* <li className={
                 (3 == this.state.activeLink ? "nav-item active_item" : "nav-item")}>
                 <Link to="/feed" className="nav-link"
                   onClick={() => { this.handleClick(3) }}>
                   <img className="navIcon" src="img/feed.svg" />
-                  <span className=" link-text">מידע</span>
+                  <span className=" link-text">פוסטים</span>
                 </Link>
-              </li>
+              </li> */}
 
               <li className={
                 (4 == this.state.activeLink ? "nav-item active_item" : "nav-item")}>
@@ -182,7 +183,7 @@ class App extends Component {
           <div className="appWrap">
             <header className="App-header">
               <div className="contact">
-                <form id="landingForm" className="landingForm">
+                <form id="topLandingForm" className="topLandingForm">
 
                   <div className="call"> 055-558-4718 &#9742;</div>
                   <input name="email" className="inputsHeader" type="email" placeholder="מייל" required></input>
@@ -203,36 +204,27 @@ class App extends Component {
             </header>
             <hr />
             <Switch>
-              <Route exact path="/">
-                <div className="projectsFlexWrapper">
-                  {
-                    this.state.projects.map((project, index) => {
-                      return (
-                        <Project className="proj" key={index} projectObj={project} />
-                      )
-                    })
-                  }
-
-                </div>
-
-              </Route>
+              {/* <Route exact path="/">
+             
+                main
+              </Route> */}
               <Route exact path="/Contact">
                 <div className="contactComponent">
-                  <form id="landingForm" className="mainGridItem landingForm formComponent">
+                  <form id="landingForm" className="landingForm">
                     <div className="formHead">יצירת קשר</div>
                     <input name="email" className="inputs" type="email" placeholder="מייל" required></input>
                     <input name="name" className="inputs" type="text" placeholder="שם" required></input>
                     <input name="phone" className="inputs" type="tel" placeholder="טלפון" required></input>
                     <textarea name="concept" className="inputs" type="text" placeholder="טקסט חופשי..."></textarea>
                     <button className="inputs submit" type="submit">שלח/י</button>
-                    <div id="thankYou" className="thankYou"></div>
+
                   </form>
                 </div>
 
               </Route>
               <Route exact path="/Manager">
                 <div className="managerPage">
-                  <form id="landingForm" className="landingForm formComponent" onSubmit={this.savePost}>
+                  <form id="landingForm" className="landingForm" onSubmit={this.savePost}>
                     <div className="formHead">הוספת פוסט חדש</div>
                     <input name="headerPost" className="inputs" type="text" placeholder="כותרת" ></input>
                     <input name="subject_sub_header" className="inputs" type="text" placeholder="נושא - כותרת משננית" ></input>
@@ -246,12 +238,189 @@ class App extends Component {
                 </div>
 
               </Route>
+              <Route exact path="/">
+                <div className="projectsFlexWrapper">
+         
+                  <div className='projCard'
+                  //  data-aos="fade-up"
+                   >
+                    <div className="projDesc" style={{ direction: 'ltr', opacity: '1' }}>
+                      <h1 style={{ fontWeight: 'bold', color: 'var(--firstColor)' }} className="shimmer appTitle">title</h1>
+                      <h3 style={{ fontWeight: 'bold', color: 'var(--secondColor)' }}>category</h3>
+
+                      <h3 style={{ fontWeight: 'bold' }}>singleDesc</h3>
+
+                    </div>
+                    <div className="imge"
+                      style={{
+                        backgroundColor: 'var(--firstColorOpacity)',
+                        minHeight: '200px'
+                      }}>
+
+                      {/* style={{ */}
+                      {/*   backgroundImage: 'url(' + this.props.projectObj.img + ')',
+                    backgroundSize: 'cover',
+                     backgroundRepeat: 'no-repeat',
+                     backgroundPosition: 'center',
+                     margin: 'auto'
+                    }}
+                     > */}
+
+
+
+                      <div className="btns">
+                        <a target="_blank" href="#">
+                          <div className="link"> <span className="shimmer">
+                            Read More</span></div></a>
+                        <a target="_blank" href="#">
+                          <div className="link link2 ">Share</div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='projCard'
+                  //  data-aos="fade-up"
+                   >
+                    <div className="projDesc" style={{ direction: 'ltr', opacity: '1' }}>
+                      <h1 style={{ fontWeight: 'bold', color: 'var(--firstColor)' }} className="shimmer appTitle">title</h1>
+                      <h3 style={{ fontWeight: 'bold', color: 'var(--secondColor)' }}>category</h3>
+
+                      <h3 style={{ fontWeight: 'bold' }}>singleDesc</h3>
+
+                    </div>
+                    <div className="imge"
+                      style={{
+                        backgroundColor: 'var(--firstColorOpacity)',
+                        minHeight: '200px'
+                      }}>
+
+                      {/* style={{ */}
+                      {/*   backgroundImage: 'url(' + this.props.projectObj.img + ')',
+                    backgroundSize: 'cover',
+                     backgroundRepeat: 'no-repeat',
+                     backgroundPosition: 'center',
+                     margin: 'auto'
+                    }}
+                     > */}
+
+
+
+                      <div className="btns">
+                        <a target="_blank" href="#">
+                          <div className="link"> <span className="shimmer">
+                            Read More</span></div></a>
+                        <a target="_blank" href="#">
+                          <div className="link link2 ">Share</div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='projCard'
+                  //  data-aos="fade-up"
+                   >
+                    <div className="projDesc" style={{ direction: 'ltr', opacity: '1' }}>
+                      <h1 style={{ fontWeight: 'bold', color: 'var(--firstColor)' }} className="shimmer appTitle">title</h1>
+                      <h3 style={{ fontWeight: 'bold', color: 'var(--secondColor)' }}>category</h3>
+
+                      <h3 style={{ fontWeight: 'bold' }}>singleDesc</h3>
+
+                    </div>
+                    <div className="imge"
+                      style={{
+                        backgroundColor: 'var(--firstColorOpacity)',
+                        minHeight: '200px'
+                      }}>
+
+                      {/* style={{ */}
+                      {/*   backgroundImage: 'url(' + this.props.projectObj.img + ')',
+                    backgroundSize: 'cover',
+                     backgroundRepeat: 'no-repeat',
+                     backgroundPosition: 'center',
+                     margin: 'auto'
+                    }}
+                     > */}
+
+
+
+                      <div className="btns">
+                        <a target="_blank" href="#">
+                          <div className="link"> <span className="shimmer">
+                            Read More</span></div></a>
+                        <a target="_blank" href="#">
+                          <div className="link link2 ">Share</div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='projCard'
+                  //  data-aos="fade-up"
+                   >
+                    <div className="projDesc" style={{ direction: 'ltr', opacity: '1' }}>
+                      <h1 style={{ fontWeight: 'bold', color: 'var(--firstColor)' }} className="shimmer appTitle">title</h1>
+                      <h3 style={{ fontWeight: 'bold', color: 'var(--secondColor)' }}>category</h3>
+
+                      <h3 style={{ fontWeight: 'bold' }}>singleDesc</h3>
+
+                    </div>
+                    <div className="imge"
+                      style={{
+                        backgroundColor: 'var(--firstColorOpacity)',
+                        minHeight: '200px'
+                      }}>
+
+                      {/* style={{ */}
+                      {/*   backgroundImage: 'url(' + this.props.projectObj.img + ')',
+                    backgroundSize: 'cover',
+                     backgroundRepeat: 'no-repeat',
+                     backgroundPosition: 'center',
+                     margin: 'auto'
+                    }}
+                     > */}
+
+
+
+                      <div className="btns">
+                        <a target="_blank" href="#">
+                          <div className="link"> <span className="shimmer">
+                            Read More</span></div></a>
+                        <a target="_blank" href="#">
+                          <div className="link link2 ">Share</div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </Route>
+              <Route exact path="/about">
+                <div className="aboutPage">
+                  <h1> עמותת מוסרים באהבה היא עמותה ללא מטרת רווח .</h1>
+                  <h2>עיקר פעילותה במתן בגדים למשפחות נזקקקות, תלושי מזון,תמיכה כלכלית .</h2>
+                  <h3>
+                    עמותת 'מוסרים באהבה' הוקמה על-מנת לסייע למיעוטי-היכולת בעיר
+
+                    מטרת העל של עמותת 'מוסרים באהבה' היא בעצם להגשים את היום-יום של אנשים מעוטי-יכולת, באמצעות כמה וכמה דרכים והזדמנויות.
+
+
+                    איך עושים זאת?
+                    פה אנו נזדקק לעזרתכם! בגדים משומשים - כן, כן, שמעתם נכון, הבגדים האלו, שכבר אין בהם צורך - הם הגשמת חלום קטן לילד קטן, למשפחה שקשיי היום-יום מנעו ממנה את הדברים הכל-כך חשובים ובנאליים בחיים.
+
+
+                    אם זה קורס באנגלית, או מוצרי חשמל ועד הדבר הכל-כך חשוב: סלי מזון וארוחות חמות בחגים.
+                  </h3>
+                  <h4> מוזמנים להיכנס לעמוד הפייסבוק של העמותה בקישור
+                    <a href="#"> מוסרים באהבה פייסבוק </a>
+                  </h4>
+                </div>
+
+              </Route>
+
             </Switch>
 
 
           </div>
         </Router>
-      </div>
+      </div >
 
     );
   }
